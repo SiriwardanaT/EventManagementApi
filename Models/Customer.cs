609a312ea45f396fba0420098a6882e48da0fcce
JANINDU MALEESHA <janindumaleesha99@gmail.com>
2023-10-14 17:09:29 +0530

    database connected

---
+++ Models/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesManagementAPI.Models
+{
+    public partial class Customer
+    {
+        public Customer()
+        {
+            TicketCustomer = new HashSet<TicketCustomer>();
+        }
+
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Nic { get; set; }
+        public string Phone { get; set; }
+
+        public virtual ICollection<TicketCustomer> TicketCustomer { get; set; }
+    }
+}
---
+++ Models/Ticket.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SalesManagementAPI.Models
+{
+    public partial class Ticket
+    {
+        public Ticket()
+        {
+            TicketCustomer = new HashSet<TicketCustomer>();
+        }
+
+        public Guid Id { get; set; }
+        public string TicketCode { get; set; }
+        public int? TicketNo { get; set; }
+        public int? TicketPrice { get; set; }
+        public int? TicketType { get; set; }
+        public string SaleStatus { get; set; }
+        public DateTime? ExDate { get; set; }
+        public int? NoOfTicket { get; set; }
+        public Guid? EventId { get; set; }
+
+        public virtual Event Event { get; set; }
+        public virtual ICollection<TicketCustomer> TicketCustomer { get; set; }
+    }
+}
